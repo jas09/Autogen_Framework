c3446f37e46d38c4027b79490e21a0702dac8d19
jas09 <azharulla.mohammed1701@gmail.com>
2025-08-26 05:25:04 +0530

    ok

--- basics2_AboutImage.py
+++ basics2_AboutImage.py
@@ -6,8 +6,10 @@ from autogen_agentchat.messages import MultiModalMessage
 from autogen_agentchat.ui import Console
 from autogen_core import Image
 from autogen_ext.models.openai import OpenAIChatCompletionClient
-#Provide OPENAI_API_KEY
-os.environ["OPENAI_API_KEY"]= "OPENAI_API_KEY"
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 async def main():
     openai_model_client = OpenAIChatCompletionClient(
         model="gpt-4o-mini",
