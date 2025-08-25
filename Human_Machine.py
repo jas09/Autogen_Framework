0bb3736aa85dfc305d07f3f3005c68eedf376d91
jas09 <azharulla.mohammed1701@gmail.com>
2025-08-26 05:04:38 +0530

    Removed API Key

--- Human_Machine.py
+++ Human_Machine.py
@@ -6,8 +6,8 @@ from autogen_agentchat.conditions import TextMentionTermination
 from autogen_agentchat.teams import RoundRobinGroupChat
 from autogen_agentchat.ui import Console
 from autogen_ext.models.openai import OpenAIChatCompletionClient
-
-os.environ["OPENAI_API_KEY"]= "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"
+#Provide OPENAI_API_KEY
+os.environ["OPENAI_API_KEY"]= "OPENAI_API_KEY"
 async def main():
     openai_model_client = OpenAIChatCompletionClient(
         model="gpt-4o-mini",
